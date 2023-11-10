ffe4610994a4bf390f972bf203c4f23924950d79
fionam23 <144394184+fionam23@users.noreply.github.com>
2023-11-10 14:02:58 -0500

    Update Main.java

--- SlimeFightSimulator/src/Main.java
+++ SlimeFightSimulator/src/Main.java
@@ -1,8 +1,14 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        int random = (int) (Math.random()*10+1);
+        System.out.println(random);
+
+
         Scanner s = new Scanner(System.in);
 
         System.out.println("Welcome to your Slime Fighting Journey.");
@@ -10,6 +16,23 @@ public class Main {
         System.out.println("Type p for Pyro slime, c for Cryo slime, h for Hydro slime, or e for Electro slime.");
         String chosenStarter = s.nextLine();
         UserStats us = new UserStats();
-        new GenerateSlime(us.getUserLevel());
+
+        Slime mySlime = new GenerateSlime(us.getUserLevel(), chosenStarter).getNewSlime();
+        System.out.println("Your adopted slime has stats of: "+mySlime.getHealth()+" HP, Type: "+mySlime.getType());
+        System.out.println("Would you like to name your slime? y/n");
+        if(s.nextLine().equalsIgnoreCase("y")){
+            System.out.println("What name?");
+            mySlime.setSlimeName(s.nextLine());
+        }
+        System.out.println("Your Slime's name is.."+mySlime.slimeName);
+        System.out.println("This is your journey to be the very best! You and your new buddy "+mySlime.slimeName+" depart from home to become the Ultimate Slime Master.");
+        System.out.println("You head out to Wispy Meadows. To walk around click enter. Maybe you will encounter a new teammate.");
+
+        if(s.nextLine().equals("")){
+            System.out.println("Walked forward. Nothing here...");
+        }
+
+
+
     }
 }
