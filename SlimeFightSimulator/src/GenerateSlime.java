9f92c0ef8f2da6db14c0f0496d25d1b900fcc347
fionam23 <144394184+fionam23@users.noreply.github.com>
2023-11-10 14:02:41 -0500

    Update GenerateSlime.java

--- SlimeFightSimulator/src/GenerateSlime.java
+++ SlimeFightSimulator/src/GenerateSlime.java
@@ -1,7 +1,13 @@
 public class GenerateSlime {
+
+
     private int level;
     private int health;
     private int attackPower;
+
+    private String type;
+
+    private Slime newSlime;
     private int rarityMultiplier;
 
     int random0to4 = (int) Math.random()*10;
@@ -10,11 +16,22 @@ public class GenerateSlime {
     //multipliers
 
 
-    public GenerateSlime(int userLevel) {
-        level = 50 * userLevel;
-        setHealth();
-        setAttackPower();
-
+    public GenerateSlime(int userLevel, String typ) {
+        if(typ.equalsIgnoreCase("p")){
+            type = "Pyro";
+        } else if(typ.equalsIgnoreCase("c")){
+            type = "Cryo";
+        } else if(typ.equalsIgnoreCase("h")){
+            type = "Hydro";
+        } else if(typ.equalsIgnoreCase("e")){
+            type = "Electro";
+        } else if(typ.equalsIgnoreCase("s")){
+            type = "Shadow";
+        } else if (typ.equalsIgnoreCase("l")) {
+            type = "Light";
+        }
+        newSlime = new Slime(type,10*(userLevel*10), 10*(userLevel));
+        newSlime.setSlimeName(""+type+" Slime");
     }
 
     private void setHealth(){
@@ -27,6 +44,11 @@ public class GenerateSlime {
     public void setAll(){
         setHealth();
         setAttackPower();
+    }
+
+
+    public Slime getNewSlime(){
+        return newSlime;
     }
 
 }
